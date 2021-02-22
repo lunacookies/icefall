e9584dc3a6584c6318c72c4d53400c8bf9228ffc
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-02-22 22:49:21 +1100

    Set colour of magit tag names

--- src/imp.rs
+++ src/imp.rs
@@ -306,6 +306,7 @@ fn syntax_highlighting(builder: &mut ThemeBuilder, palette: &Palette) {
     builder.add_rule(Textmate("magit.subheader"), palette.blue());
     builder.add_rule(Semantic("magit-ref-name"), palette.cyan());
     builder.add_rule(Semantic("magit-remote-ref-name"), palette.green());
+    builder.add_rule(Semantic("magit-tag-name"), palette.blue_2());
     builder.add_rule(Textmate("magit.entity"), palette.purple());
 
     builder.add_rule(Semantic("*.mutable"), FontStyle::Italic);
