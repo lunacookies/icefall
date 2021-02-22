d659add4a48bafd5babd639d9b8add1e5a266e12
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-02-22 21:13:01 +1100

    Don’t highlight self keyword separately

--- src/imp.rs
+++ src/imp.rs
@@ -116,7 +116,6 @@ fn syntax_highlighting(builder: &mut ThemeBuilder, palette: &Palette) {
     builder.add_rule(Semantic("builtinType"), palette.blue());
 
     builder.add_rule(Semantic("macro"), palette.green());
-    builder.add_rule(Semantic("selfKeyword"), palette.green());
     builder.add_rule(Semantic("operator.controlFlow"), palette.green());
     builder.add_rule(Semantic("typeParameter"), palette.green());
 
